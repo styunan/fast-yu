df17f64fec2c5407dd4e3d4a357467324c743141
huyunan <18842920976@163.com>
2021-02-06 22:50:53 +0800

    2342434

--- bin/fast-yu.js
+++ bin/fast-yu.js
@@ -1,43 +1,44 @@
-const fs = require("fs")
-const ejs = require("ejs")
-const util = require("util")
-const mkdirp = require("mkdirp")
-const readline = require("readline")
-const path = require("path")
-const program = require("commander")
+#!/usr/bin/env node
 
-const VERSION = require("./package").version
-const MODE_0666 = parseInt("0666", 8)
-const MODE_0755 = parseInt("0755", 8)
+const fs = require("fs");
+const ejs = require("ejs");
+const util = require("util");
+const mkdirp = require("mkdirp");
+const readline = require("readline");
+const path = require("path");
+const program = require("commander");
 
-var _exit = process.exit
-process.exit = exit
+const VERSION = require("../package").version;
+const MODE_0666 = parseInt("0666", 8);
+const MODE_0755 = parseInt("0755", 8);
+
+var _exit = process.exit;
+process.exit = exit;
 
 // CLI
+around(program, "optionMissingArgument", function (fn, args) {
+  program.outputHelp();
+  fn.apply(this, args);
+  return { args: [], unknown: [] };
+});
 
-around(program, 'optionMissingArgument', function (fn, args) {
-  program.outputHelp()
-  fn.apply(this, args)
-  return { args: [], unknown: [] }
-})
-
-before(program, 'outputHelp', function () {
+before(program, "outputHelp", function () {
   // track if help was shown for unknown option
-  this._helpShown = true
-})
+  this._helpShown = true;
+});
 
-before(program, 'unknownOption', function () {
+before(program, "unknownOption", function () {
   // allow unknown options if help was shown, to prevent trailing error
-  this._allowUnknownOption = this._helpShown
+  this._allowUnknownOption = this._helpShown;
 
   // show help if not yet shown
   if (!this._helpShown) {
-    program.outputHelp()
+    program.outputHelp();
   }
-})
+});
 
 program
-  .name("fast-yu")
+  .name("fast")
   .version(VERSION, "    --version")
   .usage("[options] [dir]")
   .option(
@@ -60,7 +61,7 @@ program
     "add View <engine> support (express|node|es6) (defaults to express)"
   )
   .option("-f, --force", "force on non-empty directory")
-  .parse(process.argv)
+  .parse(process.argv);
 
 if (!exit.exited) {
   main()
@@ -71,34 +72,34 @@ if (!exit.exited) {
  */
 function main() {
   // Path
-  var destinationPath = program.args.shift() || "."
+  var destinationPath = program.args.shift() || ".";
   // App name
-  var appName = createAppName(path.resolve(destinationPath)) || "hello-world"
+  var appName = createAppName(path.resolve(destinationPath)) || "hello-world";
 
   // Default view engine
   const options = program.opts();
-  if ((options.view === true)) {
-    if (options.express) options.view = "express"
-    if (options.node) options.view = "node"
-    if (options.es6) options.view = "es6"
+  if (options.view === true) {
+    if (options.express) options.view = "express";
+    if (options.node) options.view = "node";
+    if (options.es6) options.view = "es6";
   }
 
   // Generate application
   emptyDirectory(destinationPath, function (empty) {
     if (empty || options.force) {
-      createApplication(appName, destinationPath)
+      createApplication(appName, destinationPath);
     } else {
       confirm("destination is not empty, continue? [y/N] ", function (ok) {
         if (ok) {
-          process.stdin.destroy()
-          createApplication(appName, destinationPath)
+          process.stdin.destroy();
+          createApplication(appName, destinationPath);
         } else {
-          console.error("aborting")
-          exit(1)
+          console.error("aborting");
+          exit(1);
         }
-      })
+      });
     }
-  })
+  });
 }
 
 /**
@@ -122,39 +123,39 @@ function createApplication(name, dir) {
     author: "",
     license: "ISC",
     dependencies: {},
-  }
+  };
   // JavaScript
-  var app = loadTemplate("js/app.js")
+  var app = loadTemplate("js/app.js");
   // App modules
-  app.locals.mounts = []
+  app.locals.mounts = [];
 
   const options = program.opts();
   // Template support
   switch (options.view) {
     case "express":
-      app.locals.view = { engine: "express" }
-      pkg.dependencies.express = "^4.17.1"
-      break
+      app.locals.view = { engine: "express" };
+      pkg.dependencies.express = "^4.17.1";
+      break;
     case "node":
-      app.locals.view = { engine: "node" }
-      break
+      app.locals.view = { engine: "node" };
+      break;
     case "es6":
-      app.locals.view = { engine: "es6" }
-      break
+      app.locals.view = { engine: "es6" };
+      break;
     default:
-      app.locals.view = { engine: "world" }
-      break
+      app.locals.view = { engine: "world" };
+      break;
   }
 
   if (dir !== ".") {
-    mkdir(dir, ".")
+    mkdir(dir, ".");
   }
 
-  mkdir(dir, "public")
+  // mkdir(dir, "public")
 
   // write files
-  write(path.join(dir, "index.js"), app.render())
-  write(path.join(dir, "package.json"), JSON.stringify(pkg, null, 2) + "\n")
+  write(path.join(dir, "index.js"), app.render());
+  write(path.join(dir, "package.json"), JSON.stringify(pkg, null, 2) + "\n");
 }
 
 /**
@@ -168,9 +169,9 @@ function renamedOption(originalName, newName) {
   return function (val) {
     warning(
       util.format("option `%s' has been renamed to `%s'", originalName, newName)
-    )
-    return val
-  }
+    );
+    return val;
+  };
 }
 
 /**
@@ -184,7 +185,7 @@ function createAppName(pathName) {
     .basename(pathName)
     .replace(/[^A-Za-z0-9.-]+/g, "-")
     .replace(/^[-_.]+|-+$/g, "")
-    .toLowerCase()
+    .toLowerCase();
 }
 
 /**
@@ -196,9 +197,9 @@ function createAppName(pathName) {
 
 function emptyDirectory(dir, fn) {
   fs.readdir(dir, function (err, files) {
-    if (err && err.code !== "ENOENT") throw err
-    fn(!files || !files.length)
-  })
+    if (err && err.code !== "ENOENT") throw err;
+    fn(!files || !files.length);
+  });
 }
 
 /**
@@ -207,21 +208,21 @@ function emptyDirectory(dir, fn) {
 
 function loadTemplate(name) {
   var contents = fs.readFileSync(
-    path.join(__dirname, "templates", name + ".ejs"),
+    path.join(__dirname, "..", "templates", name + ".ejs"),
     "utf-8"
-  )
-  var locals = Object.create(null)
+  );
+  var locals = Object.create(null);
 
   function render() {
     return ejs.render(contents, locals, {
       escape: util.inspect,
-    })
+    });
   }
 
   return {
     locals: locals,
     render: render,
-  }
+  };
 }
 
 /**
@@ -232,10 +233,10 @@ function loadTemplate(name) {
  */
 
 function mkdir(base, dir) {
-  var loc = path.join(base, dir)
+  var loc = path.join(base, dir);
 
-  console.log("   \x1b[36mcreate\x1b[0m : " + loc + path.sep)
-  mkdirp.sync(loc, MODE_0755)
+  console.log("   \x1b[36mcreate\x1b[0m : " + loc + path.sep);
+  mkdirp.sync(loc, MODE_0755);
 }
 
 /**
@@ -246,8 +247,8 @@ function mkdir(base, dir) {
  */
 
 function write(file, str, mode) {
-  fs.writeFileSync(file, str, { mode: mode || MODE_0666 })
-  console.log("   \x1b[36mcreate\x1b[0m : " + file)
+  fs.writeFileSync(file, str, { mode: mode || MODE_0666 });
+  console.log("   \x1b[36mcreate\x1b[0m : " + file);
 }
 
 /**
@@ -258,12 +259,12 @@ function confirm(msg, callback) {
   var rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
-  })
+  });
 
   rl.question(msg, function (input) {
-    rl.close()
-    callback(/^y|yes|ok|true$/i.test(input))
-  })
+    rl.close();
+    callback(/^y|yes|ok|true$/i.test(input));
+  });
 }
 
 /**
@@ -272,24 +273,22 @@ function confirm(msg, callback) {
 
 function exit(code) {
   // flush output for Node.js Windows pipe bug
-  // https://github.com/joyent/node/issues/6247 is just one bug example
-  // https://github.com/visionmedia/mocha/issues/333 has a good discussion
   function done() {
-    if (!draining--) _exit(code)
+    if (!draining--) _exit(code);
   }
 
-  var draining = 0
-  var streams = [process.stdout, process.stderr]
+  var draining = 0;
+  var streams = [process.stdout, process.stderr];
 
-  exit.exited = true
+  exit.exited = true;
 
   streams.forEach(function (stream) {
     // submit empty write request and wait for completion
-    draining += 1
-    stream.write("", done)
-  })
+    draining += 1;
+    stream.write("", done);
+  });
 
-  done()
+  done();
 }
 
 /**
@@ -299,36 +298,36 @@ function exit(code) {
  */
 
 function warning(message) {
-  console.error()
+  console.error();
   message.split("\n").forEach(function (line) {
-    console.error("  warning: %s", line)
-  })
-  console.error()
+    console.error("  warning: %s", line);
+  });
+  console.error();
 }
 
 /**
  * Install an around function; AOP.
  */
 
-function around (obj, method, fn) {
-  var old = obj[method]
+function around(obj, method, fn) {
+  var old = obj[method];
 
   obj[method] = function () {
-    var args = new Array(arguments.length)
-    for (var i = 0; i < args.length; i++) args[i] = arguments[i]
-    return fn.call(this, old, args)
-  }
+    var args = new Array(arguments.length);
+    for (var i = 0; i < args.length; i++) args[i] = arguments[i];
+    return fn.call(this, old, args);
+  };
 }
 
 /**
  * Install a before function; AOP.
  */
 
-function before (obj, method, fn) {
-  var old = obj[method]
+function before(obj, method, fn) {
+  var old = obj[method];
 
   obj[method] = function () {
-    fn.call(this)
-    old.apply(this, arguments)
-  }
+    fn.call(this);
+    old.apply(this, arguments);
+  };
 }
